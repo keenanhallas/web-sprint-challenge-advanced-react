bca185e5aaf606d1692a5d4adb3e0f5d9f5ea725
Keenan <keenanhallas@gmail.com>
2020-06-12 18:37:19 -0500

    useForm hook working & handling changes - not handling submit(yet)

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (initialState) => {
+    const [formState, setFormState] = useState(initialState);
+
+    const handleChanges = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+        console.log(formState);
+      };
+
+    return [formState, handleChanges];
+}
