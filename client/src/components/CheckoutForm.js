7e197eeae5ce6899db2b653eb2378af84eaac60e
Keenan <keenanhallas@gmail.com>
2020-06-12 19:10:08 -0500

    tests working, but could be improved - mvp done

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -26,13 +26,14 @@ const CheckoutForm = (props) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <h2>Checkout Form</h2>
+        <h2 data-testid="checkoutForm">Checkout Form</h2>
         <label>
           First Name:
           <input
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+            data-testid="firstName"
           />
         </label>
         <label>
@@ -41,6 +42,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            data-testid="lastName"
           />
         </label>
         <label>
@@ -49,25 +51,26 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            data-testid="address"
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges} data-testid="city" />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges} data-testid="state" />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges} data-testid="zip" />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkoutButton">Checkout</button>
       </form>
 
       {showSuccessMessage && (
-        <div className="success-message" data-testid="successMessage">
+        <div className="success-message" data-testid="successMessage" data-testid="successMessage"> 
           <p>
             You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
           </p>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByTestId} = render(<CheckoutForm />);
+    const formHeader = getByTestId(/checkoutForm/i);
+    const firstNameField = getByTestId(/firstName/i);
+    fireEvent.change(firstNameField, { target: { value: 'checkout form' } });
+    expect(formHeader).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByTestId} = render(<CheckoutForm />);
+
+    const firstName = getByTestId(/firstName/i);
+    const lastName = getByTestId(/lastName/i);
+    const address = getByTestId(/address/i);
+    const city = getByTestId(/city/i);
+    const state = getByTestId(/state/i);
+    const zip = getByTestId(/zip/i);
+    const checkoutButton = getByTestId(/checkoutButton/i);
+    //const successMessage = getByTestId(/successMessage/i); this fails because the element isn't mounted yet - I'm sure there's a better way to test this
+
+    fireEvent.change(firstName, { target: { value: 'Keenan' } });
+    fireEvent.change(lastName, { target: { value: 'Hallas' } });
+    fireEvent.change(address, { target: { value: '123 Main St.' } });
+    fireEvent.change(city, { target: { value: 'Louisville' } });
+    fireEvent.change(state, { target: { value: 'Kentucky' } });
+    fireEvent.change(zip, { target: { value: '12345' } });
+
+    fireEvent.click(checkoutButton);
+    const successMessage = getByTestId(/successMessage/i);
+});
